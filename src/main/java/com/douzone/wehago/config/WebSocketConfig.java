da9d9e224061e6e4326d73394cd13397ba781e0a
kimskyyyy <hhhhh823@naver.com>
2023-08-15 15:18:49 +0900

    Feat: websocket

--- src/main/java/com/douzone/wehago/config/WebSocketConfig.java
+++ src/main/java/com/douzone/wehago/config/WebSocketConfig.java
@@ -1,2 +1,27 @@
-package com.douzone.wehago.config;public class WebSocketConfig {
+package com.douzone.wehago.config;
+
+import com.douzone.wehago.webSocket.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+
+/*
+    WebSocketHandler 활성화를 위한 Configuration
+    @EnableWbSocket : WebSocket 활성화
+ */
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/check")
+                .setAllowedOrigins("*");
+
+    }
 }
--- src/main/java/com/douzone/wehago/webSocket/WebSocketHandler.java
+++ src/main/java/com/douzone/wehago/webSocket/WebSocketHandler.java
@@ -1,2 +1,27 @@
-package com.douzone.wehago.webSocket;public class WebSocketHandler {
+package com.douzone.wehago.webSocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/*
+    server-client 1:n 관계
+    한 서버에 여러 클라이언트가 접속할 수 있으므로 여러 클라이언트가 전송한 메시지를 받아 처리할 Handler 필요
+    클라이언트로 받은 메시지를 콘솔에 출력하고 클라이언트에게 "웹소켓 테스트" 메시지를 전송함
+ */
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+
+        String input = message.getPayload();
+        log.info(input);
+        TextMessage textMessage = new TextMessage("웹소켓 테스트");
+        session.sendMessage(textMessage);
+    }
 }
